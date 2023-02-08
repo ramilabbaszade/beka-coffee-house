f1debc099dbd405aea848ed8951a366caa978707
ramilabbaszade <abbasovramil65@gmail.com>
2023-02-09 00:58:46 +0100

    feat: basket functionalities

--- src/pages/Basket.vue
+++ src/pages/Basket.vue
@@ -5,13 +5,34 @@ const store = inject("store")
 </script>
 
 <template>
-  <div>
-    <div v-for="product in store.basket">
-      {{product.name}}
+  <div class="basket_container">
+    <div class="basket_head">BASKET: {{ store.totalPrice }}$</div>
+    <div class="product_item__container" v-for="product in store.basket">
+      <div class="product_item">
+        <div class="product_item__left">{{product.name}}</div>
+        <div class="product_item__right">
+          {{product.price}}$ <button @click="store.removeFromBasket(product)">remove</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-
+  .basket_container{
+    max-width: 1200px;
+    margin: 0 auto;
+    display: grid;
+  }
+  .basket_head{
+    font-size: 2rem;
+    margin: 2rem 0;
+  }
+  .product_item{
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid black;
+    margin-bottom: 1rem;
+    padding-bottom: 1rem;
+  }
 </style>
--- src/store.js
+++ src/store.js
@@ -5,9 +5,18 @@ export const store = reactive({
     count:0,
     totalPrice:0,
     addToBasket(product) {
-        this.basket.push(product)
+        const randomNum = Math.floor(Math.random() * 1000)
+        const newProduct = {...product, pId:randomNum}
+        this.basket.push(newProduct)
         this.totalPrice+=product.price
         this.count++
+    },
+    removeFromBasket(product) {
+        console.log(product)
+        console.log(this.basket)
+        this.basket = this.basket.filter(item=> item.pId !== product.pId)
+        this.totalPrice-=product.price
+        this.count--
     }
 
 })
